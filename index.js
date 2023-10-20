f83adcabc457c1caa07322b2f58b246a0b8c2f3a
Joison Ferreira <joisonfs@gmail.com>
2023-10-20 14:50:42 -0300

    Versao inicial

---
+++ index.js
@@ -0,0 +1,20 @@
+var setaDireita = window.document.getElementById("seta-direita")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+
+var leonardo = window.document.getElementById("leonardo")
+var samanta = window.document.getElementById("samanta")
+var bruna = window.document.getElementById("bruna")
+
+function RolarParaDireita(){
+  leonardo.style = "display:none"
+  bruna.style = "display:flex"
+  setaDireita.style = "display:none"
+  setaEsquerda.style = "display:flex"
+}
+
+function RolarParaEsquerda(){
+  leonardo.style = "display:flex"
+  bruna.style = "display:none"
+  setaDireita.style = "display:flex"
+  setaEsquerda.style = "display:none"
+}
